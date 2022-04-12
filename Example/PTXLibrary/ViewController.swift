b584e1adf84f5c5b4964134e42f249dd502dbdf7
pan <664023725@qq.com>
2022-04-12 11:53:35 +0800

    add PTXClass

--- Example/PTXLibrary/ViewController.swift
+++ Example/PTXLibrary/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import PTXLibrary
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,5 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func testAction(_ sender: Any) {
+        PTXClass.showMessage()
+    }
 }
 
---
+++ PTXLibrary/Classes/PTXClass.swift
@@ -0,0 +1,16 @@
+//
+//  PTXClass.swift
+//  PTXLibrary
+//
+//  Created by YYKT0035 on 2022/4/12.
+//
+
+import UIKit
+
+public class PTXClass: NSObject {
+
+    public static func showMessage() {
+        print("------>:showMessage in PTXClass")
+    }
+    
+}
